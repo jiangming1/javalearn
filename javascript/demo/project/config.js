b57528b8ed78da2dfdf0d9a1695f70db08ce3490
p88health <p88health@p88healths-Mac-mini.local>
2018-09-30 20:52:58 +0800

    a

---
+++ javascript/demo/project/config.js
@@ -0,0 +1,12 @@
+const config = {
+  port: 3001,
+  database: {
+    DATABASE: 'koa_demo',
+    USERNAME: 'root',
+    PASSWORD: 'my-secret-1pw',
+    PORT: '33060',
+    HOST: '21.caiwuhao.com'
+  }
+}
+
+module.exports = config
